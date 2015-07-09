5dd273fd474a4823b09fb079dcb8bd6b2a9e8c0b
EragonJ <eragonj@eragonj.me>
2015-07-09 13:55:50 +0800

    fixed #164, use try-catch to prevent 'cant find module' error message

--- src/backend/main.js
+++ src/backend/main.js
@@ -2,10 +2,16 @@
 window.requireNode = window.require;
 
 // auto reload
+var gulp;
 var remote = requireNode('remote');
-var gulp = requireNode('gulp');
 
-// There is no gulp in production build
+try {
+  gulp = requireNode('gulp');
+} catch(e) {
+  // There is no gulp in production build, so we have to use
+  // try-catch to throw errors in console for this
+}
+
 if (gulp) {
   gulp.task('reload', function() {
     remote.getCurrentWindow().reload();
