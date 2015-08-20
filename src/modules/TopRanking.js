bb2d581d743c565e127c5a95d88065c11284ab26
EragonJ <eragonj@eragonj.me>
2015-08-20 23:19:02 +0800

    fixed #251, add support for choosing different TopRankings !

--- src/modules/TopRanking.js
+++ src/modules/TopRanking.js
@@ -1,47 +1,77 @@
 var request = require('request');
+var iTunesRSSData = require('node-itunes-rss-data');
 var BaseModule = require('../modules/BaseModule');
 var BaseTrack = require('../models/track/BaseTrack');
+var EventEmitter = require('events').EventEmitter;
 
-var TopRanking = BaseModule(function() {
+var TopRanking = function() {
+  EventEmitter.call(this);
+
+  // Right now, there is only one source
   this._selectedSource = 'itunes';
+  this._selectedCountry = 'us';
   this._sources = {
     itunes: {
-      url:
-        'http://itunes.apple.com/rss/topsongs/limit=100/explicit=true/json',
-      json: true
+      url: 'https://itunes.apple.com/__country__/rss/topsongs/limit=100/explicit=true/json'
     }
   };
-});
+};
 
-TopRanking.prototype.setSource = function(source) {
-  this._selectedSource = source;
+TopRanking.prototype = Object.create(EventEmitter.prototype);
+TopRanking.constructor = TopRanking;
+
+TopRanking.prototype._getSourceURL = function() {
+  var rawURL = this._sources[this._selectedSource].url;
+  return rawURL.replace('__country__', this._selectedCountry);
+};
+
+TopRanking.prototype.getCountryList = function() {
+  /*
+   * {
+      "ae": "United Arab Emirates",
+      "ag": "Antigua and Barbuda",
+      "ai": "Anguilla",
+      "al": "Albania",
+      ...
+     }
+   */
+  return iTunesRSSData.countries;
+};
+
+TopRanking.prototype.changeCountry = function(countryCode) {
+  if (countryCode in iTunesRSSData.countries) {
+    this._selectedCountry = countryCode;
+    this.emit('topRanking-changed', countryCode);
+  }
 };
 
 TopRanking.prototype.get = function() {
-  var promise = new Promise(function(resolve, reject) {
-    request.get(this._sources[this._selectedSource],
-      function(error, response, data) {
-        if (error) {
-          resolve([]);
-        }
-        else {
-          var entries = data.feed.entry || [];
-          var result = entries.map(function(entry) {
-            var track = new BaseTrack({
-              title: entry['im:name'].label,
-              artist: entry['im:artist'].label,
-              covers: {
-                default: entry['im:image'][1].label,
-                medium: entry['im:image'][2].label,
-                large: entry['im:image'][2].label
-              }
-            });
-            return track;
+  var promise = new Promise((resolve, reject) => {
+    request.get({
+      url: this._getSourceURL(),
+      json: true
+    }, function(error, response, data) {
+      if (error) {
+        resolve([]);
+      }
+      else {
+        var entries = data.feed.entry || [];
+        var result = entries.map(function(entry) {
+          var track = new BaseTrack({
+            title: entry['im:name'].label,
+            artist: entry['im:artist'].label,
+            covers: {
+              default: entry['im:image'][1].label,
+              medium: entry['im:image'][2].label,
+              large: entry['im:image'][2].label
+            }
           });
-          resolve(result);
-        }
+          return track;
+        });
+        resolve(result);
+      }
     });
-  }.bind(this));
+  });
   return promise;
 };
 
