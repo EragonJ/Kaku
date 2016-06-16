daedf2a49a128cb5d7a134c082896c1205b82e00
Peng-Jie <dean.leehom@gmail.com>
2016-06-16 15:31:35 +0800

    Add emoji picker

--- src/views/components/chatroom/comment/comment-form.js
+++ src/views/components/chatroom/comment/comment-form.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import ActionButton from '../../shared/action-button';
+import EmojiPicker from 'react-simple-emoji';
 
 class CommentForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { text: '', showSelector: false };
+    this._handleEmoji = this._handleEmoji.bind(this);
+    this._selectEmoji = this._selectEmoji.bind(this);
   }
 
   _onSubmit(e) {
     e.preventDefault();
-    var text = this.refs.text.value.trim();
-    this.refs.text.value = '';
+    const text = this.refs.text.value.trim();
+    this.setState({ text: '' });
 
     // let its parent know which comment is sent
     this.props.onSubmit(text);
+  }
+
+  _selectEmoji() {
+    this.setState({ showSelector: !this.state.showSelector });
+  }
+
+  _handleInputChange(e) {
+    const text = e.target.value;
+    this.setState({ text });
+  }
+
+  _handleEmoji(emojiText) {
+    const inpuText = this.state.text;
+    this.setState({ text: `${inpuText}:${emojiText}:` });
   }
 
   componentDidUpdate() {
@@ -34,13 +52,25 @@ class CommentForm extends React.Component {
     return (
       <form className="comment-form form-inline" onSubmit={onSubmit}>
         <div className="form-group">
-          <input className="form-control" type="text" ref="text"/>
+          <input
+            className="form-control"
+            type="text"
+            ref="text"
+            value={this.state.text}
+            onChange={(e) => this._handleInputChange(e)}
+          />
         </div>
         <ActionButton
           buttonClass="btn btn-primary"
           type="submit"
           l10nId="chartoom_comment_form_submit"
-          isDisabled={!isRoomConnected}/>
+          isDisabled={!isRoomConnected}
+        />
+        <EmojiPicker
+          show={this.state.showSelector}
+          selector={() => this._selectEmoji()}
+          handleEmoji={(e) => this._handleEmoji(e)}
+        />
       </form>
     );
     /* jshint ignore:end */
