fb5ba521b5dae2eee0aa1778fcdedcf98e6c87fd
Peng Jie <dean.leehom@gmail.com>
2016-06-21 10:59:27 +0800

    Kaku emoji picker (#401)
    * Add emoji picker
    
    * Update react-simple-emoji version and fix

--- src/views/components/chatroom/comment/comment-form.js
+++ src/views/components/chatroom/comment/comment-form.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ActionButton from '../../shared/action-button';
+import EmojiPicker from 'react-simple-emoji';
 
 class CommentForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { text: '', showSelector: false };
+    this._handleEmoji = this._handleEmoji.bind(this);
+    this._selectEmoji = this._selectEmoji.bind(this);
+    this._handleInputChange = this._handleInputChange.bind(this);
   }
 
   _onSubmit(e) {
     e.preventDefault();
-    var text = this.refs.text.value.trim();
-    this.refs.text.value = '';
+    const text = this.refs.text.value.trim();
+    this.setState({ text: '' });
 
     // let its parent know which comment is sent
     this.props.onSubmit(text);
+  }
+
+  _selectEmoji() {
+    this.setState({ showSelector: !this.state.showSelector });
+  }
+
+  _handleInputChange(e) {
+    const text = e.target.value;
+    this.setState({ text });
+  }
+
+  _handleEmoji(emojiText) {
+    const inpuText = this.state.text;
+    this.setState({
+      text: `${inpuText}:${emojiText}:`,
+      showSelector: !this.state.showSelector,
+    });
   }
 
   componentDidUpdate() {
@@ -34,13 +56,25 @@ class CommentForm extends React.Component {
     return (
       <form className="comment-form form-inline" onSubmit={onSubmit}>
         <div className="form-group">
-          <input className="form-control" type="text" ref="text"/>
+          <input
+            className="form-control"
+            type="text"
+            ref="text"
+            value={this.state.text}
+            onChange={this._handleInputChange}
+          />
         </div>
         <ActionButton
           buttonClass="btn btn-primary"
           type="submit"
           l10nId="chartoom_comment_form_submit"
-          isDisabled={!isRoomConnected}/>
+          isDisabled={!isRoomConnected}
+        />
+        <EmojiPicker
+          show={this.state.showSelector}
+          selector={this._selectEmoji}
+          handleEmoji={this._handleEmoji}
+        />
       </form>
     );
     /* jshint ignore:end */
