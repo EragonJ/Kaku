dace3599918da6f62178606f6ac771e544eb2128
Peng Jie <dean.leehom@gmail.com>
2016-08-17 16:00:33 +0800

    rewrite src/modules/DownloadManager.js with ES6 syntax

--- src/modules/DownloadManager.js
+++ src/modules/DownloadManager.js
@@ -2,49 +2,47 @@
 // 1. handle multiple downloads
 // 2. support pause / canel downloads
 // ... etc
-var Fs = require('fs');
-var Request = require('request');
-var RequestProgress = require('request-progress');
-var EventEmitter = require('events').EventEmitter;
+const Fs = require('fs');
+const Request = require('request');
+const RequestProgress = require('request-progress');
+const { EventEmitter } = require('events');
 
-function DownloadManager() {
-  EventEmitter.call(this);
+class DownloadManager extends EventEmitter {
+  constructor() {
+    super();
+    this._downloads = [];
+  }
 
-  this._downloads = [];
+  download(src, path) {
+    const req = RequestProgress(Request.get(src), {
+      delay: 1000 // start to emit after 1000ms delay
+    });
+
+    req.on('progress', (state) => {
+      this.emit('download-progress', state.percent);
+    })
+    .on('error', (error) => {
+      console.log('error when requesting file');
+      console.log(error);
+      this.emit('download-error');
+    })
+    .pipe(Fs.createWriteStream(path))
+    .on('error', (error) => {
+      console.log('error when saving file to path' + path);
+      console.log(error);
+      this.emit('download-error');
+    })
+    .on('close', () => {
+      const index = this._downloads.indexOf(req);
+      if (index >= 0) {
+        this._downloads.splice(index, 1);
+      }
+      this.emit('download-finish');
+    });
+
+    this._downloads.push(req);
+    return req;
+  }
 }
-
-DownloadManager.prototype = Object.create(EventEmitter.prototype);
-DownloadManager.constructor = DownloadManager;
-
-DownloadManager.prototype.download = function(src, path) {
-  var req = RequestProgress(Request.get(src), {
-    delay: 1000 // start to emit after 1000ms delay
-  });
-
-  req.on('progress', (state) => {
-    this.emit('download-progress', state.percent);
-  })
-  .on('error', (error) => {
-    console.log('error when requesting file');
-    console.log(error);
-    this.emit('download-error');
-  })
-  .pipe(Fs.createWriteStream(path))
-  .on('error', (error) => {
-    console.log('error when saving file to path' + path);
-    console.log(error);
-    this.emit('download-error');
-  })
-  .on('close', () => {
-    var index = this._downloads.indexOf(req);
-    if (index >= 0) {
-      this._downloads.splice(index, 1);
-    }
-    this.emit('download-finish');
-  });
-   
-  this._downloads.push(req);
-  return req;
-};
 
 module.exports = new DownloadManager();
