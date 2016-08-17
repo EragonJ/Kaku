ef09b606637a09e4595a7bf8ebf73914a12402f9
Peng Jie <dean.leehom@gmail.com>
2016-08-17 16:01:21 +0800

    rewrite src/modules/NewsFetcher.js with ES6 syntax

--- src/modules/NewsFetcher.js
+++ src/modules/NewsFetcher.js
@@ -1,23 +1,25 @@
-var Request = require('request');
+const Request = require('request');
 
-function NewsFetcher() {
-  this._newsLink = 'http://kaku.rocks/news.json';
-}
+class NewsFetcher {
+  constructor() {
+    this._newsLink = 'http://kaku.rocks/news.json';
+  }
 
-NewsFetcher.prototype.get = function() {
-  var promise = new Promise((resolve, reject) => {
-    Request.get(this._newsLink, (error, response, body) => {
-      if (error) {
-        reject(error);
-        console.log(error);
-      }
-      else {
-        var result = JSON.parse(body);
-        resolve(result.news);
-      }
+  get() {
+    const promise = new Promise((resolve, reject) => {
+      Request.get(this._newsLink, (error, response, body) => {
+        if (error) {
+          reject(error);
+          console.log(error);
+        }
+        else {
+          var result = JSON.parse(body);
+          resolve(result.news);
+        }
+      });
     });
-  });
-  return promise;
-};
+    return promise;
+  }
+}
 
 module.exports = new NewsFetcher();
