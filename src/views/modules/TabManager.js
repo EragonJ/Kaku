ba7de7e02edbfdea270b6c19ea580dee1d751e14
EragonJ <eragonj@eragonj.me>
2016-08-24 16:22:45 +0800

    fixed TabManager

--- src/views/modules/TabManager.js
+++ src/views/modules/TabManager.js
@@ -5,23 +5,25 @@ class TabManager extends EventEmitter {
     super();
 
     // default tab is home
-    this._currentTab = 'home';
+    this._tabName = 'home';
+    this._tabOptions = undefined;
 
     Object.defineProperty(TabManager, 'currentTab', {
       enumerable: true,
       configurable: false,
       get() {
-        return this._currentTab;
+        return this._tabName;
       }
     });
   }
 
   setTab(tabName, tabOptions) {
-    if (tabName === this._currentTab) {
+    if (tabName === this._tabName && tabOptions === this._tabOptions) {
       return;
     }
     else {
-      this._currentTab = tabName;
+      this._tabName = tabName;
+      this._tabOptions = tabOptions;
       this.emit('changed', tabName, tabOptions);
     }
   }
