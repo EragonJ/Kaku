58945c969603551f3392c23d61903455774c8a41
Peng-Jie <dean.leehom@gmail.com>
2017-02-25 12:08:03 +0800

    rewrite src/views/modules/KonamiCodeManager.js with ES6 syntax
    rewrite src/views/modules/Notifier.js with ES6 syntax

--- src/views/modules/KonamiCodeManager.js
+++ src/views/modules/KonamiCodeManager.js
@@ -1,34 +1,36 @@
-function KonamiCodeManager() {
-  this._pattern = "38384040373937396665";
-  this._keyCodeCache = '';
-  this._callback = () => {};
-  this._boundCheckKeyCodePattern = this._checkKeyCodePattern.bind(this);
-}
-
-KonamiCodeManager.prototype.attach = function(root, callback) {
-  // TODO
-  // not sure whether we should support multi-elements in this case
-  if (root instanceof Element) {
-    root.removeEventListener('keydown', this._boundCheckKeyCodePattern);
-    root.addEventListener('keydown', this._boundCheckKeyCodePattern);
-    this._callback = callback;
+class KonamiCodeManager {
+  constructor() {
+    this._pattern = "38384040373937396665";
+    this._keyCodeCache = '';
+    this._callback = () => {};
+    this._boundCheckKeyCodePattern = this._checkKeyCodePattern.bind(this);
   }
-};
 
-KonamiCodeManager.prototype._checkKeyCodePattern = function(e) {
-  if (e) {
-    this._keyCodeCache += e.keyCode;
-    if (this._keyCodeCache.length === this._pattern.length) {
-      if (this._keyCodeCache === this._pattern) {
-        console.log('KonamiCode passed, let\'s show some easter eggs :)');
-        this._callback();
+  attach(root, callback) {
+    // TODO
+    // not sure whether we should support multi-elements in this case
+    if (root instanceof Element) {
+      root.removeEventListener('keydown', this._boundCheckKeyCodePattern);
+      root.addEventListener('keydown', this._boundCheckKeyCodePattern);
+      this._callback = callback;
+    }
+  }
+
+  _checkKeyCodePattern(e) {
+    if (e) {
+      this._keyCodeCache += e.keyCode;
+      if (this._keyCodeCache.length === this._pattern.length) {
+        if (this._keyCodeCache === this._pattern) {
+          console.log('KonamiCode passed, let\'s show some easter eggs :)');
+          this._callback();
+        }
+        this._keyCodeCache = '';
       }
-      this._keyCodeCache = '';
-    }
-    else if (!this._pattern.match(this._keyCodeCache)) {
-      this._keyCodeCache = '';
+      else if (!this._pattern.match(this._keyCodeCache)) {
+        this._keyCodeCache = '';
+      }
     }
   }
-};
+}
 
 module.exports = new KonamiCodeManager();
